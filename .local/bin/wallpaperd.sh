9e13cdc494c5b87a15c64cdd3af56a607cfdceb9
at2er <xb0515@outlook.com>
2025-07-05 15:25:21 +0800

    feat: waybar.

---
+++ .local/bin/wallpaperd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bg_dir=~/Pictures/wallpaper
+interval="10m"
+
+options="-m fill"
+
+killall swaybg
+
+swaybg -i "$(find "$bg_dir" -type f | shuf -n 1)" ${options}&
+old_pid=$!
+
+while true; do
+	sleep $interval
+	swaybg -i "$(find "$bg_dir" -type f | shuf -n 1)" ${options}&
+	new_pid=$!
+	sleep 1s
+	kill $old_pid
+	old_pid=$new_pid
+done
