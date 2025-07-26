876f350c3174db74087c6f0edcb3139f3bb127a4
at2er <xb0515@outlook.com>
2025-07-26 10:14:22 +0800

    feat(script): startdwl

--- .local/bin/wallpaperd.sh
+++ .local/bin/wallpaperd.sh
@@ -5,18 +5,29 @@ interval="10m"
 
 options="-m fill"
 
-killall swaybg
-others=$(pgrep wallpaperd.sh | grep -v $$)
-kill $others
+daemon_mode() {
+	killall swaybg
+	others=$(pgrep wallpaperd.sh | grep -v $$)
+	kill $others
 
-swaybg -i "$(find "$bg_dir" -type f | shuf -n 1)" ${options}&
-old_pid=$!
-
-while true; do
-	sleep $interval
 	swaybg -i "$(find "$bg_dir" -type f | shuf -n 1)" ${options}&
-	new_pid=$!
-	sleep 1s
-	kill $old_pid
-	old_pid=$new_pid
-done
+	old_pid=$!
+	while true; do
+		sleep $interval
+		swaybg -i "$(find "$bg_dir" -type f | shuf -n 1)" ${options}&
+		new_pid=$!
+		sleep 1s
+		kill $old_pid
+		old_pid=$new_pid
+	done
+}
+
+switch() {
+	local cur="$2"
+}
+
+case "$1" in
+	daemon) daemon_mode ;;
+	switch) switch "$2" ;;
+	*) echo Unsupport option! ;;
+esac
